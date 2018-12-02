cba4fe6f18f089bd83807e962e780792d2d97898
Hamoud Khatar <skhamoud@gmail.com>
2018-12-02 23:36:38 +0000

    prettier format

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ httpServer.listen(httpPort, function() {
 	console.log(`HTTP Server now listening on port ${httpPort}...`);
 });
 
-// Setup https server 
+// Setup https server
 const httpsOptions = {
-  key: fs.readFileSync('./https/key.pem'),
-  cert: fs.readFileSync('./https/cert.pem')
-}
+	key: fs.readFileSync('./https/key.pem'),
+	cert: fs.readFileSync('./https/cert.pem')
+};
 const httpsServer = https.createServer(httpsOptions, function(req, res) {
 	serversHandler(req, res, 'https');
 });
@@ -28,7 +28,7 @@ httpsServer.listen(httpsPort, function() {
 /** Handles the meat of server logic */
 function serversHandler(req, res, protocol = 'http') {
 	// Get url and parse it
-	const parsedUrl = new URL( req.url, `${protocol}://localhost:${config[protocol+'Port']}` );
+	const parsedUrl = new URL(req.url, `${protocol}://localhost:${config[protocol + 'Port']}`	);
 
 	// Get the path, trimmed down from starting and ending slashes
 	const path = parsedUrl.pathname.replace(/^\/+|\/+$/g, '');
@@ -44,12 +44,12 @@ function serversHandler(req, res, protocol = 'http') {
 
 	// Get the payload if any
 	const decoder = new StringDecoder('utf-8');
-  let reqPayload = '';
-  
+	let reqPayload = '';
+
 	req.on('data', function(chunk) {
 		reqPayload += decoder.write(chunk);
-  });
-  
+	});
+
 	req.on('end', () => {
 		reqPayload += decoder.end();
 		const reqData = {
@@ -58,17 +58,17 @@ function serversHandler(req, res, protocol = 'http') {
 			headers,
 			query,
 			body: reqPayload
-    };
-    
+		};
+
 		// figure out the handler
-    const routeHandler = router[path] ? router[path] : router.notFound;
-    
+		const routeHandler = router[path] ? router[path] : router.notFound;
+
 		// route the request to handler
 		routeHandler(reqData, function(statusCode, payload) {
 			// setup the status codes and Send the response
 			statusCode = typeof statusCode === 'number' ? statusCode : 200;
-      payload = typeof payload === 'object' ? payload : {};
-      res.setHeader('Content-Type','application/json')
+			payload = typeof payload === 'object' ? payload : {};
+			res.setHeader('Content-Type', 'application/json');
 			res.writeHead(statusCode);
 			res.end(JSON.stringify(payload));
 		});
